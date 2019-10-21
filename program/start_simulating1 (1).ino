81ad2a257e2110638a012918e88d708e86056884
amitmaurya1029 <55481714+amitmaurya1029@users.noreply.github.com>
2019-10-21 23:21:15 +0530

    Update start_simulating1 (1).ino

--- program/start_simulating1 (1).ino
+++ program/start_simulating1 (1).ino
@@ -1,10 +1,21 @@
+float i;
 void setup()
 {pinMode(10,OUTPUT);
-
+pinMode(6,INPUT);
 }
 
 void loop()
 {
-  digitalWrite(10,HIGH);  
-  delay(10); // Delay a little bit to improve simulation performance
+  i=analogRead(6);
+ if(i<3)
+ {digitalWrite(10,HIGH);  
+  delay(500);
+  digitalWrite(10,LOW);
+  delay(500);}
+   else if(i>500)
+ {digitalWrite(10,HIGH);  
+  delay(100);
+  digitalWrite(10,LOW);
+  delay(100);
+ }
 }
